9161b3720f006344513ca3af9299cad1daff41fc
Péter Kalicz <kaliczp@gmail.com>
2023-10-17 14:55:14 +0200

    Import precipitation
    readxl did not import character mixed numerics in the right way.

--- import.R
+++ import.R
@@ -1,3 +1,14 @@
 library(readxl)
 bot <- as.data.frame(read_excel("Teljes Botankert 1980-2023.xlsx"))
 summary(bot)
+
+## A csv-elmentett adatok beolvasása
+botok <- read.csv2("Botan.csv")
+botok[botok[,8] == "nyom", 8]  <- "0,05"
+botok[botok[,8] == "ny", 8] <- "0,05"
+botok[botok[,8] == "-", 8] <- ""
+## Csak ellenőrzés
+ttmp <- sub(",", ".", botok[!botok[,8] == "",8])
+which(is.na(as.numeric(ttmp)))
+
+botok[,8] <- as.numeric(sub(",", ".", botok[,8]))
