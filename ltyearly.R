97ea3fe5380989e79cb0d2be239b8352db144ab3
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-31 18:27:24 +0100

    Create and save yearly means and sums

--- ltyearly.R
+++ ltyearly.R
@@ -8,3 +8,8 @@ ltdates <- seq.Date(as.Date("1870-01-01"), as.Date("2021-12-31"), by = "day")
 library(xts)
 Krcz.xts <- xts(cbind(temp = rawKrczt[,2], prec = rawKrczPrec[,2]), ltdates)
 
+## Yearly means and sums
+KrczYear.xts <- cbind(apply.yearly(Krcz.xts$temp, mean),
+                      apply.yearly(Krcz.xts$prec, sum))
+## Export csv
+write.zoo(KrczYear.xts, "KrczYear.csv", sep = ";", dec = ",")
