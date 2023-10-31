02eee368b8189a2934cef3bd685372a18318842e
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-10-31 18:05:28 +0100

    LT time-series created

--- ltyearly.R
+++ ltyearly.R
@@ -1,2 +1,10 @@
-rawKrczt <- read.csv2("t_h_sopron_18702021.csv", dec = ".")
-rawKrczPrec <- read.csv2("r_h_sopron_18702021.csv", dec = ".")
+## Import raw
+rawKrczt <- read.csv2("t_h_sopron_18702021.csv", dec = ".")[,1:2]
+rawKrczPrec <- read.csv2("r_h_sopron_18702021.csv", dec = ".")[,1:2]
+
+## Create dates
+ltdates <- seq.Date(as.Date("1870-01-01"), as.Date("2021-12-31"), by = "day")
+## Create time-series
+library(xts)
+Krcz.xts <- xts(cbind(temp = rawKrczt[,2], prec = rawKrczPrec[,2]), ltdates)
+
