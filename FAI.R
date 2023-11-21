b0c1d83c5b603ae2dc7f65ed3b62011b54629b6f
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-21 18:04:34 +0100

    First working FAI

--- FAI.R
+++ FAI.R
@@ -7,17 +7,26 @@
 
 FAI <- function(x) {
     ## Test index is.Date
-    
     ## Temperature preparation
-    temp.index <- 
-    tempwindow <- period.apply()
-    ## select even windwos
-    temp7.8 <- tempwindow[]
-    ## Precipitation preparation
-    prec.sum.raw
-    prec.sum.summer <- prec.sum.raw
+    temp.monthly.mean <- apply.monthly(x$temp, mean)
+    ## select july aug
+    july.index <- seq(7, length(temp.monthly.mean), by = 12)
+    july.mean <- temp.monthly.mean[july.index]
+    aug.mean <- temp.monthly.mean[july.index + 1]
+    temp.df <- data.frame(July = july.mean, Aug = aug.mean)
+    temp7.8 <- rowMeans(temp.df)
+    ## ## Precipitation preparation
+    prec.monthly.sum <- apply.monthly(x$prec, sum)
+    prec.df <- data.frame(May = prec.monthly.sum[july.index - 2],
+                          Jun = prec.monthly.sum[july.index - 1],
+                          July = prec.monthly.sum[july.index],
+                          July2 = prec.monthly.sum[july.index],
+                          Aug = prec.monthly.sum[july.index + 1]
+                          )
+    prec.sum <- rowSums(prec.df)
     ## FAI
-    100 * temp / prec.sum.summer
+    100 * (temp7.8 / prec.sum)
 }
 
-FAI(Krcz.xts)
+Krcz.FAI <- data.frame(Year = 1870:2021, FAI(Krcz.xts))
+plot(Krcz.FAI, type = "l")
